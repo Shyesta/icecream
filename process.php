edc992bb8bbb9d9f00283682a6cd0a794310f60e
Sage Bain <bain.sage@student.greenriver.edu>
2024-01-09 13:25:36 -0500

    Added order summary

--- process.php
+++ process.php
@@ -9,6 +9,19 @@
 </head>
 <body>
     <h1>Thank you for your order!</h1>
+    <?php
+    echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $scoops = $_POST['scoops'];
+        $flavors = $_POST['flavor'];
+        $flavorString = implode(", ", $flavors);
+        $cone = $_POST['cone'];
+
+        echo "$<p>$scoops scoops</p>";
+        echo "<p>Flavors: $flavorString</p>";
+        echo "<p>Cone: $cone</p>";
+    ?>
 </body>
 </html>
-<?php
